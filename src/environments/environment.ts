d2d14ebc0abb90f76f593481464aa4b523ac493f
Thomas Li <tomshli@live.com>
2025-06-22 16:56:10 -0400

    build and styling changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   protectedResourceMap: new Map<string, Array<string>>([
       ["https://boraboraai-api.azurewebsites.net/txt2img", ['api://d949f2eb-c6b9-4ff4-a48c-db819ac07578/a1111.post.txt2img']],
       ["https://boraboraai-api.azurewebsites.net/img2img", ['api://d949f2eb-c6b9-4ff4-a48c-db819ac07578/a1111.post.img2img']],
-  ]),
+  ])
 };
 
 /*
